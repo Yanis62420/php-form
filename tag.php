aa30fde5ffc084afd9b3f2312161d0cb27c00acb
[Julien Sacher] <[julien.sacher@hotmail.fr]>
2020-11-23 17:30:59 +0100

    Ajout d'une page qui montre les balises php
    Cette page permet de montrer quand on doit utiliser la balise

---
+++ tag.php
@@ -0,0 +1,20 @@
+<?php
+
+// code php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo 'lorem ipsum' ?></p>
+    <p><?= 'lorem ipsum' ?></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus reprehenderit beatae sed fugiat laborum non, quod voluptate sunt, dolorum odit facere ipsa similique mollitia? Id animi deleniti iure magni illo?</p>
+</body>
+</html>
+<?php
+
+// pas de balise fermante si votre vode php termine le fichier
